c1f24923de1f29f803661946addf66b89570a683
braniubojni <erik95harutyunyan@gmail.com>
2023-08-14 13:12:39 +0400

    eh/feat: roles bus-logic implemented

--- src/roles/roles.service.ts
+++ src/roles/roles.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { Role } from './roles.model';
 
 @Injectable()
-export class RolesService {}
+export class RolesService {
+  constructor(@InjectModel(Role) private roleRepo: typeof Role) {}
+
+  async create(dto: CreateRoleDto) {
+    const role = await this.roleRepo.create(dto);
+    return role;
+  }
+
+  async getByValue(value: string) {
+    const role = await this.roleRepo.findOne({ where: { value } });
+    return role;
+  }
+}
